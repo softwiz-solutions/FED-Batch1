6052e34ed22fd4f583fd0cefeeda0fa8f0df441e
Abdul_Hanan <abdulhanan3279@gmail.com>
2023-05-25 18:09:13 +0500

    js variables

---
+++ LearnJavascript/script.js
@@ -0,0 +1,17 @@
+// // document.write("<h1>welcome to very first class of js</h1>")
+// //variable declaration
+// var penHolder = "12", cardHolder = 13
+
+// // varibale initialization
+// // penHolder=12;
+
+// function localVariable() {
+//     penHolder = "usama";
+//     console.log(penHolder)
+// }
+// localVariable()
+// console.log(penHolder)
+
+// a+b -->expression
+// a,b operands
+//+ operator
