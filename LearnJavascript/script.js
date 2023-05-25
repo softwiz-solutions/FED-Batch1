d4d54b9a812e2f61407a94b4627661470ada6377
Abdul_Hanan <abdulhanan3279@gmail.com>
2023-05-25 19:16:33 +0500

    if elese

--- LearnJavascript/script.js
+++ LearnJavascript/script.js
@@ -15,3 +15,95 @@
 // a+b -->expression
 // a,b operands
 //+ operator
+
+
+//Chapter # 06, OPERATORS
+
+// 1. increment operator
+var a = 1;
+console.log("simple a ", a)
+// a++; //a =a+1
+// console.log("inc a ", a)
+// a--; //a =a-1
+// console.log(" dec a ", a)
+
+// prefix, postfix
+// prefix: --a, ++a,
+//postfix: a++,a--,
+
+
+//prefix
+// var b;
+// b = --a
+// console.log("a=", a);
+// console.log("b=", b);
+
+//ppost
+// var b;
+// b = a--
+// console.log("a=", a);
+// console.log("b=", b);
+
+// a = 1
+
+
+// a += 5 //a = a + 5
+// a *= 5 //a = a * 5
+
+// console.log(a++ + --a + a++ - a--)
+// console.log(a)
+
+
+
+
+a = 5
+// if (a == 1) {
+//     //code
+//     return "hello"
+// }
+// else if () {
+
+// }
+// else {
+//     //code
+// }
+
+// conditional operator (? :)
+
+// a == 1 ? console.log("hello") : console.log("not hello")
+// var result = a == 1 ? "<h1>hello</h1>" : (a == 2) ? "hello2 " : "<h1>not hello</h1>"
+
+// // var result = a == 2 ? "hello" : a > 5 ? "hy" : a < 2 ? "not hello" : 'hy ali';
+
+// console.log("result=", result)
+// console.log(typeof a)
+// var result = (typeof a == "number" ? "a is number" : "a is not number")
+// console.log("result", result)
+
+//conditions
+
+
+// var number = 1
+
+// if (number == 1) {
+//     console.log("hello")
+// }
+// else {
+//     console.log("not hello");
+// }
+
+// nested if
+// number == 6
+// if (number > 5) {
+//     if (number == 1) {
+
+//         console.log("number is 1");
+//     }
+//     else {
+//         console.log("number is greater than 1");
+//     }
+// }
+// else {
+//     console.log("number is less than 5");
+// }
+
